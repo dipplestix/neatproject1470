38c1a4c6b852630ae78f3ae1a844d4024420c329
dipplestix <chrism315@gmail.com>
2021-12-03 02:14:42 -0500

    Removed print statements

--- src/xor/xor.py
+++ src/xor/xor.py
@@ -5,8 +5,6 @@ def get_fitness(model):
     inputs = torch.tensor([[0., 0.], [0., 1.], [1., 0.], [1., 1.]])
     outputs = model(inputs)
     expected = torch.tensor([0., 1., 1., 0.])
-    print(outputs - expected)
-    print(torch.square(outputs-expected))
     error = torch.sum(torch.square(outputs-expected))
     
     return -error
