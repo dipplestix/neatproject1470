cce6aa4ca4f9fd8ba6909188727952930853f84b
Chris Mascioli <chrism315@gmail.com>
2021-12-04 16:42:28 -0500

    Update test_make_network.py

--- src/tests/test_make_network.py
+++ src/tests/test_make_network.py
@@ -10,26 +10,26 @@ genome_1 = Genome([gen1], 12)
 genome_2 = Genome([gen1, gen2], 12)
 
 
-def test_make_network_1:
+def test_make_network_1():
   m = make_network(genome_1)
   assert m(torch.tensor([0.]) == torch.sigmoid(torch.tensor(0.))
 
            
-def test_make_network_2:
+def test_make_network_2():
   m = make_network(genome_1)
   assert m(torch.tensor([1]) == torch.sigmoid(torch.tensor(100.))
 
            
-def test_make_network_3:
+def test_make_network_3():
   m = make_network(genome_2)
   assert m(torch.tensor([0]) == torch.sigmoid(torch.tensor(0.))
 
            
-def test_make_network_4:
+def test_make_network_4():
   m = make_network(genome_2)
   assert m(torch.tensor([1]) == torch.sigmoid(torch.tensor(130.))
 
           
-def test_make_network_5:
+def test_make_network_5():
   m = make_network(genome_2)
   assert m(torch.tensor([.1]) == torch.sigmoid(torch.tensor(13.))
