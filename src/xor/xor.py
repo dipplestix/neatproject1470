e09547bd251a53a407a3f8d563c8865f1b798470
Chris Mascioli <chrism315@gmail.com>
2021-12-04 13:46:36 -0500

    Update xor.py

--- src/xor/xor.py
+++ src/xor/xor.py
@@ -2,7 +2,7 @@ import torch
 
 
 def get_fitness(model):
-    inputs = torch.tensor([[0., 0.], [0., 1.], [1., 0.], [1., 1.]])
+    inputs = torch.tensor([[1., 0., 0.], [1., 0., 1.], [1., 1., 0.], [1., 1., 1.]])
     outputs = model(inputs)
     expected = torch.tensor([0., 1., 1., 0.])
     error = torch.sum(torch.square(outputs-expected))
