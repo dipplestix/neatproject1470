c0d0979ce6bdff99b179fff26ee6da8ea04c57c1
Chris Mascioli <chrism315@gmail.com>
2021-12-04 16:31:21 -0500

    Update make_phenotype.py
    rewrote phenotpye function, now no longer blows up the network

--- src/neat/make_phenotype.py
+++ src/neat/make_phenotype.py
@@ -1,7 +1,5 @@
 from neat.neat_structures import Genome, Gene
 from copy import deepcopy
-from random import random, choice, uniform, sample
-from typing import Callable, List
 import torch
 from torch import nn
 
@@ -24,16 +22,25 @@ def make_dics(connections):
 
 
 def make_network(gen: Genome):
+    # FInd what's input and output
     fwd, rev = make_dics(gen.active_connects.keys())
     outputs = [n for n in gen.nodes if n not in fwd.keys()]
     inputs = [n for n in gen.nodes if n not in rev.keys()]
-    gaps = max(outputs) - max(inputs)
+    
+    # Record what goes into each layer, the matrix, and what comes out of each layer
     layers = []
+    in_ls = []
+    out_ls = []
     nn_layers = []
+    max_node = 0
+    
+    # Go backwards and determine the network graph
     nodes_to_process = deepcopy(outputs)
     while nodes_to_process:
         next_layer = []
         for n in nodes_to_process:
+            if n > max_node:
+                max_node = n
             try:
                 next_layer.extend(rev[n])
             except KeyError:
@@ -42,63 +49,48 @@ def make_network(gen: Genome):
         nodes_to_process = next_layer
     layers.reverse()
     layers = layers[1:]
+    layers.append(outputs)
+    # Build the model
     if len(layers) > 1:
         for i in range(len(layers) - 1):
             if i == 0:
                 in_l = inputs
             else:
                 in_l = out_l
-            out_l = deepcopy(in_l)
-            out_l.extend(layers[i+1])
-            out_l = list(set(out_l))
-            # print(in_l)
-            # print(out_l)
+            out_l = layers[i+1]
             nn_layer = nn.Linear(len(in_l), len(out_l), bias=False)
-            for ni in in_l:
-                for no in out_l:
-                    in_node = ni
-                    out_node = no
+            for i, ni in enumerate(in_l):
+                for j, no in enumerate(out_l):
                     if ni == no:
                         w = 1
-                    elif no in in_l:
-                        w = 0
                     else:
                         try:
                             w = gen.active_connects[(ni, no)]
                         except KeyError:
                             w = 0
-                    if ni not in inputs:
-                        in_node -= gaps
-                    if no not in inputs:
-                        out_node -= gaps
                     with torch.no_grad():
-                        nn_layer.weight[out_node, in_node] = w
-            # print(nn_layer.weight)
+                        nn_layer.weight[j, i] = w
             nn_layers.append(nn_layer)
-            nn_layers.append(nn.ReLU(inplace=True))
+            in_ls.append(in_l)
+            out_ls.append(out_l)
     else:
         out_l = layers[0]
-    last_layer = nn.Linear(len(out_l), len(outputs), bias=False)
-    for i in out_l:
-        for o in outputs:
-            in_node = i
-            try:
-                w = gen.active_connects[(i, o)]
-            except KeyError:
-                w = 0
-            if i not in inputs:
-                in_node -= gaps
-            with torch.no_grad():
-                last_layer.weight[o-min(outputs), in_node] = w
-    # print(last_layer.weight)
-    nn_layers.append(last_layer)
-    # print(layers)
-    # print(nn_layers)
-    model = nn.Sequential(*nn_layers)
-    return model
     
-            
+    def model(val_in):
+        if len(val_in.shape) == 1:
+            val_in = torch.reshape(val_in, [1, -1])
+        assert len(val_in.shape) == 2, 'Requires 2D Tensor'
+        values = torch.zeros(val_in.shape[0], max_node+1)
+        values[:, inputs] = val_in
         
+        for i, layer in enumerate(nn_layers):
+            ins = values[:, in_ls[i]]
+            loop_value = layer(ins)
+            if i != len(nn_layers) - 1:
+                pass
+            loop_value = torch.sigmoid(loop_value)
+            values[:, out_ls[i]] = loop_value
+        
+        return values[:, outputs]
     
-
-
+    return model
